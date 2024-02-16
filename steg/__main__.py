49b953e58ca46101ea6682a636ed94dae23fa95f
hrchlhck <vpemfh7@protonmail.com>
2024-02-16 14:46:46 -0300

    refactor and add image steganography

--- steg/__main__.py
+++ steg/__main__.py
@@ -1,14 +1,35 @@
 from argparse import ArgumentParser
-from .stego import revelar, esconder
+from .stego import LSB, Tipo
+from pathlib import Path
+
+import cv2
+
+def _caminho_existe(caminho) -> bool:
+    caminho = Path(caminho)
+    return caminho.exists()
 
 PARSER = ArgumentParser("steg")
 PARSER.add_argument("-i", dest="imagem_entrada", help="Caminho para a imagem original", type=str, required=True)
 PARSER.add_argument("-o", dest="imagem_saida", help="Caminho para a imagem com texto escondido", type=str)
 PARSER.add_argument("-f", dest="funcao", choices=["esconder", "revelar"], help="Ação que deseja realizar", required=True)
+PARSER.add_argument("-t", dest="eh_texto", action='store_true', default=False)
 
 ARGS = PARSER.parse_args()
+lsb = LSB(ARGS.imagem_entrada, ARGS.imagem_saida, tipo=Tipo.TEXTO if ARGS.eh_texto else Tipo.IMAGEM)
 
 if ARGS.funcao == "esconder":
-    esconder(ARGS.imagem_entrada, ARGS.imagem_saida)
+    if ARGS.eh_texto:
+        texto = input("Digite o texto que deseja esconder: ")
+        lsb.esconder(texto)
+    else:
+        caminho_img = input("Digite o caminho da imagem que deseja esconder: ")
+
+        if not _caminho_existe(caminho_img):
+            print(f"O caminho que você especificou ({caminho_img}) é inválido.")
+            exit(1)
+
+        img = cv2.imread(caminho_img)
+        lsb.esconder(img)
+
 elif ARGS.funcao == "revelar":
-    revelar(ARGS.imagem_entrada)
+    lsb.revelar()
--- steg/stego.py
+++ steg/stego.py
@@ -1,6 +1,13 @@
-import cv2
+from enum import Enum
 
-def converte_texto_bits(texto: str) -> str:
+import cv2
+import numpy as np
+
+def int_para_bin(n: int) -> str:
+    # int para binario
+    return f"{n:0{8}b}"
+
+def texto_para_bits(texto: str) -> str:
     # char para unicode 
     texto = [ord(i) for i in texto]
 
@@ -26,85 +33,191 @@ def trata_entrada(prompt: str, tipo: type):
             exit(1)
 
         return int(dados)
-        
 
-def esconder(imagem_original: str, imagem_com_texto: str):
-    # Carregando imagem na memória
-    imagem = cv2.imread(imagem_original)
+def binario_para_int(n: str) -> int:
+    return int(n, base=2)
 
-    # Obtém dimensões da imagem
+def imagem_para_bits(imagem: np.ndarray) -> str:
+    # Os dois primeiros bytes serão utilizados para representar as dimensões da imagem
+    # de saída. Portanto, o tamanho máximo de uma imagem para ser escondida será de 
+    # 255x255.
     altura, largura, _ = imagem.shape
+    resultado = int_para_bin(altura) + int_para_bin(largura)
 
-    # Armazena entrada do usuário
-    texto = trata_entrada("Digite o texto que deseja esconder: ", str)
+    for i in range(largura):
+        for j in range(altura):
+            for ch in range(3):
+                resultado += int_para_bin(imagem[i, j, ch])
+    
+    return resultado
 
-    # Converte o texto recebido em uma lista de bits
-    bits = converte_texto_bits(texto)
+class Tipo(Enum):
+    IMAGEM = 1
+    TEXTO = 2
 
-    # Variáveis para denotar qual pixel iremos alterar
-    x = 0
-    y = 0
-    for i in range(len(bits)):
-        # Recupera bit da lista de bits e converte em inteiro
-        bit = int(bits[i])
+class LSB:
+    def __init__(self, imagem_original: str, imagem_escondida: str, tipo: Tipo=Tipo.IMAGEM) -> None:
+        self._imagem_original = imagem_original
+        self._imagem_escondida = imagem_escondida
+        self._tipo = tipo
 
-        if x < largura:
-            if bit == 1:
-                # Adicione um bit no pixel na coluna x, linha y e canal 0
-                imagem[x, y, 0] = imagem[x, y, 0] | 1
+    def _converte_dados(self, dados) -> object:
+        if self._tipo == Tipo.IMAGEM:
+            l = imagem_para_bits(dados)
+            return l
+        
+        return texto_para_bits(dados)
+
+    def _revelar_imagem(self) -> None:
+        imagem = cv2.imread(self._imagem_original)
+
+        # Recupera as dimensões da imagem (primeiros dois bytes)
+        altura_bin, largura_bin = "", ""
+        _, largura_orig, _ = imagem.shape
+        ch = 0
+        for i in range(16):
+            pixel = imagem[0, i, ch]
+
+            bit = str(pixel & 1)
+            if i < 8:
+                altura_bin += bit
             else:
-                # Remova um bit no pixel na coluna x, linha y e canal 0
-                imagem[x, y, 0] = imagem[x, y, 0] & 0xFFFFFFFE
+                largura_bin += bit
+
+            if i % 8 == 0:
+                ch += 1
+
+
+        altura = int(altura_bin,base=2)
+        largura = int(largura_bin,base=2)
+        x = 16
+        y = 0
+        bits = ""
+
+        # Mantemos a contagem de ch (canal) para continuar iterando
+        for i in range(altura*largura*8*3):
+            if x == largura_orig - 1:
+                x = 0
+                y += 1
+            
+            if ch == 3:
+                ch = 0
+
+            if x < largura_orig - 1:
+                pixel = imagem[y, x, ch]
+                bits += str(pixel & 1)
+                x += 1
+            
+            if i % 8 == 0:
+                ch += 1
         
-        # Se x for igual a largura da imagem, então passe para a próxima linha
-        # e redefina x para 0
-        if x == largura - 1:
-            y += 1
-            x = 0
+        # Converte subconjuntos de 8 em 8 bits para bytes e converte para base decimal
+        _bytes = [int(bits[i:i+8], base=2) for i in range(0, len(bits), 8)]
+        resultado = np.asarray(_bytes).reshape((altura, largura, 3)).astype(np.uint8)
+
+        # Salva imagem
+        cv2.imwrite(self._imagem_escondida, resultado)
+
+    def revelar(self):
+        if self._tipo == Tipo.IMAGEM:
+            self._revelar_imagem()
+        else:
+            self._revelar_texto()
+
+    def _revelar_texto(self):
+        # Carrega a imagem com (potencial) texto escondido
+        imagem = cv2.imread(self._imagem_original)
         
-        # Indica que todos os pixeis foram visitados
-        if y == altura:
-            print("A imagem acabou! Abortando.")
+        # Armazena a quantidade de caracteres que se deseja recuperar o valor original
+        qtde_caracter = trata_entrada("Digite a quantidade de caracteres que deseja recuperar: ", int)
+
+        # Variáveis para denotar qual pixel iremos alterar
+        x = 0
+        y = 0
+        ch = 0
+        bits = "" # Armazena os bits recuperados
+
+        altura, largura, _ = imagem.shape
+        for i in range(qtde_caracter * 8):
+            # Se x for igual a largura da imagem, então passe para a próxima linha
+            # e redefina x para 0
+            if x == largura - 1:
+                y += 1
+                x = 0
+            
+            if x < largura-1:
+                if ch == 3:
+                    ch = 0
+
+                # Recupera pixel na coluna x, linha y e canal 0
+                pixel = imagem[y, x, ch]
+
+                # Recupera bit menos significativo
+                bits += str(pixel & 1)
+
+                if i % 8 == 0:
+                    ch += 1
+
+                # Incrementa as colunas visitadas
+                x += 1
+
+            # Indica que todos os pixeis foram visitados
+            if y == altura:
+                print("A imagem acabou! Abortando.")
+                exit(1)
+        
+        print(bits)
+        print(f"Texto revelado: {converte_bits_texto(bits)}")
+
+    def esconder(self, dados) -> None:
+        if self._tipo == Tipo.IMAGEM and not isinstance(dados, np.ndarray):
+            print("O tipo dos dados de entrada está incorreto. Forneça uma imagem!")
             exit(1)
+        
+        # Carregando imagem na memória
+        imagem = cv2.imread(self._imagem_original)
 
-        # Incrementa as colunas visitadas
-        x += 1
-    print(f"Mensagem escondida: {texto} ({bits})")
-    cv2.imwrite(imagem_com_texto, imagem)
+        # Obtém dimensões da imagem
+        altura, largura, _ = imagem.shape
 
-def revelar(imagem_com_texto: str):
-    # Carrega a imagem com (potencial) texto escondido
-    imagem = cv2.imread(imagem_com_texto)
-    
-    # Armazena a quantidade de caracteres que se deseja recuperar o valor original
-    qtde_caracter = trata_entrada("Digite a quantidade de caracteres que deseja recuperar: ", int)
+        # Converte os dados de entrada em uma lista de bits
+        bits = self._converte_dados(dados)
 
-    # Variáveis para denotar qual pixel iremos alterar
-    x = 0
-    y = 0
-    bits = "" # Armazena os bits recuperados
+        # Variáveis para denotar qual pixel iremos alterar
+        x = 0
+        y = 0
+        ch = 0
+        for i in range(len(bits)):
+            # Recupera bit da lista de bits e converte em inteiro
+            bit = int(bits[i])
 
-    altura, largura, _ = imagem.shape
-    for _ in range(qtde_caracter * 8):
-        if x < largura:
-            # Recupera pixel na coluna x, linha y e canal 0
-            pixel = imagem[x, y, 0]
+            # Se x for igual a largura da imagem, então passe para a próxima linha
+            # e redefina x para 0
+            if x == largura-1:
+                y += 1
+                x = 0
 
-            # Recupera bit menos significativo
-            bits += str(pixel & 1)
+            if x < largura-1:
 
-        # Se x for igual a largura da imagem, então passe para a próxima linha
-        # e redefina x para 0
-        if x == largura - 1:
-            y += 1
-            x = 0
+                if ch == 3:
+                    ch = 0
 
-        # Indica que todos os pixeis foram visitados
-        if y == altura:
-            print("A imagem acabou! Abortando.")
-            exit(1)
+                if bit == 1:
+                    # Adicione um bit no pixel na coluna x, linha y e canal 0
+                    imagem[y, x, ch] = imagem[y, x, ch] | 1
+                else:
+                    # Remova um bit no pixel na coluna x, linha y e canal 0
+                    imagem[y, x, ch] = imagem[y, x, ch] & 0xFFFFFFFE
 
-        # Incrementa as colunas visitadas
-        x += 1
-    
-    print(f"Texto revelado: {converte_bits_texto(bits)}")
+                if i % 8 == 0:
+                    ch += 1
+
+                x += 1
+            
+            # Indica que todos os pixeis foram visitados
+            if y == altura - 1:
+                print("A imagem acabou! Abortando.")
+                exit(1)
+
+        print(f"Mensagem escondida: {dados} ({bits})")
+        cv2.imwrite(self._imagem_escondida, imagem)
